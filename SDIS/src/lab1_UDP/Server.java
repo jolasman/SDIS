d7720acd7853add677c9d527267575deaef7531a
Joel Carneiro <jolasman@hotmail.com>
2017-02-24 12:16:14 +0000

    LAB1 finished i guess

--- SDIS/src/lab1_UDP/Server.java
+++ SDIS/src/lab1_UDP/Server.java
@@ -1,13 +1,17 @@
 package lab1_UDP;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Server {
 
 	public static void main(String args[])
 	{
 		DatagramSocket sock = null;
-
+		Map<String, String> plates = new HashMap<String, String>();
 		try
 		{
 			//1. creating a server socket, parameter is local port number
@@ -26,12 +30,58 @@ public class Server {
 				sock.receive(incoming);
 				byte[] data = incoming.getData();
 				String s = new String(data, 0, incoming.getLength());
+				String t = "";
+				  
+			
+				if(s.charAt(0) == 'R'){
+					String register = s.substring(0, 10);
+					String plateRegister = s.substring(10,18);
+					char zzRegister = s.charAt(18);
+					char beforenameRegister = s.charAt(20);
+					String nomeRegister = s.substring(21,s.length()-1);
+					char endRegister = s.charAt(s.length()-1);
 
-				//echo the details of incoming data - client ip : client port - client message
-				echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+					//echo the details of incoming data - client ip : client port - client message
+					echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
 
-				s = "OK, a tua mensagem foi recebida. esta foi a tua mensagem : " + s;
-				DatagramPacket dp = new DatagramPacket(s.getBytes() , s.getBytes().length , incoming.getAddress() , incoming.getPort());
+					if((register.equals("REGISTER <")) && (plateRegister.matches("^(\\d{2}-?\\d{2}-?\\w{2})$") 
+							&& zzRegister =='>' && beforenameRegister == '<' && endRegister == '>' )){
+						t = "modo registo activado ... estamos a registar o seu pedido";
+						plates.put(plateRegister,nomeRegister);
+						
+						t += System.lineSeparator() + "registado com sucesso."  + System.lineSeparator() 
+						+ "matricula: " + plateRegister + " " + "nome: " + plates.get(plateRegister); 
+										
+					}
+				}
+				else if(s.charAt(0) == 'L'){
+					String lookup = s.substring(0, 8);
+					String plateLookup = s.substring(8,16);
+					char endLookup = s.charAt(s.length()-1);
+
+
+					//echo the details of incoming data - client ip : client port - client message
+					echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+
+					if((lookup.equals("LOOKUP <")) && (plateLookup.matches("^(\\d{2}-?\\d{2}-?\\w{2})$") && endLookup == '>' )){
+
+						t = "estamos à procura do dono da matricula e se a mesma existe....  " + plates.get(plateLookup);
+						
+						if(plates.get(plateLookup) == null){
+							t += System.lineSeparator() + " nao existe a matricula " + plateLookup + " registada.";
+						}
+						else{
+							t += System.lineSeparator() + "o dono da matricula: " + plates.get(plateLookup);						}
+							
+					}
+
+				}
+
+
+				/*else{
+					 t = "OK, a tua mensagem foi recebida. esta foi a tua mensagem : " + s;
+				}*/
+				DatagramPacket dp = new DatagramPacket(t.getBytes() , t.getBytes().length , incoming.getAddress() , incoming.getPort());
 				sock.send(dp);
 			}
 		}
