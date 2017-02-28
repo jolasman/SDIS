4766223a5137f2159f96793dc1030659303c29cf
Joel Carneiro <jolasman@hotmail.com>
2017-02-28 14:49:48 +0000

    lab2 almost done

--- SDIS/src/lab2_UDP_Multicast/MulticastSocketClient.java
+++ SDIS/src/lab2_UDP_Multicast/MulticastSocketClient.java
@@ -1,63 +1,95 @@
 package lab2_UDP_Multicast;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.UnknownHostException;
-import java.io.*;
-import java.net.*;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MulticastSocketClient {
-    
-    final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
 
-    public static void main(String[] args) throws UnknownHostException {
-        // Get the address that we are going to connect to.
-        InetAddress address = InetAddress.getByName(INET_ADDR);
-        
-        // Create a buffer of bytes, which will be used to store
-        // the incoming bytes containing the information from the server.
-        // Since the message is small here, 256 bytes should be enough.
-        byte[] buf = new byte[256];
-        
-        // Create a new Multicast socket (that will allow other sockets/programs
-        // to join it as well.
-        try (MulticastSocket clientSocket = new MulticastSocket(PORT)){
-            //Joint the Multicast group.
-            clientSocket.joinGroup(address);
-            String s;
-    		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+	final static String INET_ADDR = "224.0.0.3";
+	final static int PORT1 = 8888;
 
-     
-            while (true) {
-            	
-            	//take input and send the packet
-            	 System.out.println("Multicast --> Enter message to send : ");
-				
-				s = (String)cin.readLine();
-				byte[] b = s.getBytes();
 
-				DatagramPacket  dp = new DatagramPacket(b , b.length , address , PORT);
-				clientSocket.send(dp);
+
+	public static void main(String[] args) throws IOException {
+		String s;
+		String t;
+		BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
+
+
+		InetAddress mcastAddr = InetAddress.getByName(INET_ADDR);
+		int mcastPORT = 7777;
+
+		int PORT = PORT1;//TODO
+		byte[] buffer = new byte[65000];
+		MulticastSocket socket = new MulticastSocket(mcastPORT);
+		socket.joinGroup(mcastAddr);
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, mcastAddr, mcastPORT);
+
+		boolean not_connected = true;
+		int i = 0;
+		InetAddress srvcAddr;
+		int srvcPort;
+		String raw_data = null;
+
+		while(not_connected){
+			socket.receive(packet);
+			raw_data = new String(packet.getData(), 0, packet.getLength());
+			System.out.println(i++ + " Trying to connect\nReceived: " + raw_data);
+			srvcAddr = packet.getAddress();
+			System.out.println("multicast adress: " + srvcAddr.toString());
+			not_connected = false;
+		}
+
+
+		try{
+
+			while(true){
+				//take input and send the packet
+				echo("Multicast --> Enter message to send : ");
+				t = (String)cin.readLine();
+				byte[] b = t.getBytes();
+
+				String[] host_things = raw_data.split(" ");
+
+				DatagramPacket  dp = new DatagramPacket(b , b.length , InetAddress.getByName(host_things[0]), Integer.parseInt(host_things[1]));
+				socket.send(dp);
 
 				//now receive reply
 				//buffer to receive incoming data
-				byte[] buffer = new byte[65536];
-				DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-				clientSocket.receive(reply);
+				byte[] buffer1 = new byte[65536];
+				DatagramPacket reply = new DatagramPacket(buffer1, buffer1.length);
+				socket.receive(reply);
 
 				byte[] data = reply.getData();
 				s = new String(data, 0, reply.getLength());
 
+
+				String example = "Mensagem enviada para o servidor com sucesso: " + t;
+				echo(example);
+
 				//echo the details of incoming data - client ip : client port - client message
-				System.out.println(reply.getAddress().getHostAddress() + " : " + reply.getPort() + " - " + s);
+				echo("Mensagem recebida: (servidor e porta) "  + reply.getAddress().getHostAddress() + " : " + reply.getPort() + 
+						System.lineSeparator() + s);
 			}
-         
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+
+		}catch(IOException e)
+		{
+			System.err.println("IOException " + e);
+		}
+
+
+	}
+	public static void echo(String msg)
+	{
+		System.out.println(msg);
+	}
 }
--- SDIS/src/lab2_UDP_Multicast/MulticastSocketServer.java
+++ SDIS/src/lab2_UDP_Multicast/MulticastSocketServer.java
@@ -1,42 +1,120 @@
 package lab2_UDP_Multicast;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
- 
-public class MulticastSocketServer {
-     
-    final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
-    public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        // Get the address that we are going to connect to.
-        InetAddress addr = InetAddress.getByName(INET_ADDR);
-      
-        // Open a new DatagramSocket, which will be used to send the data.
-        try (DatagramSocket serverSocket = new DatagramSocket()) {
-        	
-			//buffer to receive incoming data
-			byte[] buffer = new byte[65536];
-			DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
-			//2. Wait for an incoming data
-			System.out.println("Multicast Server socket created. Waiting for incoming data...");
-			//communication loop
-			while(true)
-			{
-				serverSocket.receive(incoming);
-				byte[] data = incoming.getData();
-				String s = new String(data, 0, incoming.getLength());
-				
+import java.net.MulticastSocket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-				DatagramPacket dp = new DatagramPacket(s.getBytes() , s.getBytes().length ,
-						incoming.getAddress() , incoming.getPort());
-				serverSocket.send(dp);
+public class MulticastSocketServer {
+	final static String INET_ADDR = "224.0.0.3";
+	final static int PORT1 = 8888;
+
+	public static void main(String[] args) throws IOException {
+		Map<String, String> plates = new HashMap<String, String>();
+
+		int PORT = PORT1;
+		InetAddress mcastAddr = InetAddress.getByName(INET_ADDR);
+		int mcastPORT = 7777;
+		byte[] buffer = new byte[65000];
+		InetAddress hostAddr = InetAddress.getLocalHost();
+		MulticastSocket socket = new MulticastSocket(PORT);
+		socket.joinGroup(mcastAddr);
+		DatagramPacket packet = new DatagramPacket(buffer, 65000, hostAddr, PORT);
+
+		//executor for sending hello every 1sec
+		String outMessage = hostAddr.getHostAddress().toString() + " " + PORT;
+		System.out.println(outMessage);
+		byte[] autoBuffer = outMessage.getBytes();
+		DatagramPacket autoPacket = new DatagramPacket(autoBuffer, autoBuffer.length, mcastAddr, mcastPORT);
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+		Runnable task = () -> {
+			try {
+				socket.send(autoPacket);
+
+				System.out.println("Sending multicast with message: " + " (Host Adress + PORT) " + outMessage);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Failed to multicast");
+				e.printStackTrace();
 			}
-                
-            
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+		};
+
+		int initialDelay = 0;
+		int period = 10;
+		executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+
+		//escutar se algum cliente está a mandar dados
+		System.out.println("Server started");
+		
+		
+		
+		while (true) {
+			
+			socket.receive(packet);
+			byte[] data = packet.getData();
+			String s = new String(data, 0, packet.getLength());
+			String t = "";
+
+			if(s.charAt(0) == 'R'){
+				String register = s.substring(0, 10);
+				String plateRegister = s.substring(10,18);
+				char zzRegister = s.charAt(18);
+				char beforenameRegister = s.charAt(20);
+				String nomeRegister = s.substring(21,s.length()-1);
+				char endRegister = s.charAt(s.length()-1);
+				//echo the details of incoming data - client ip : client port - client message
+				echo(packet.getAddress().getHostAddress() + " : " + packet.getPort() + " - " + s);
+
+				if((register.equals("REGISTER <")) && (plateRegister.matches("^(\\d{2}-?\\d{2}-?\\w{2})$") 
+						&& zzRegister =='>' && beforenameRegister == '<' && endRegister == '>' )){
+					t = "modo registo activado ... estamos a registar o seu pedido";
+					plates.put(plateRegister,nomeRegister);
+
+					t += System.lineSeparator() + "registado com sucesso."  + System.lineSeparator() 
+					+ "matricula: " + plateRegister + " " + "nome: " + plates.get(plateRegister); 					
+				}
+			}
+			if(s.charAt(0) == 'L'){
+				String lookup = s.substring(0, 8);
+				String plateLookup = s.substring(8,16);
+				char endLookup = s.charAt(s.length()-1);
+
+
+				//echo the details of incoming data - client ip : client port - client message
+				echo(packet.getAddress().getHostAddress() + " : " + packet.getPort() + " - " + s);
+
+				if((lookup.equals("LOOKUP <")) && (plateLookup.matches("^(\\d{2}-?\\d{2}-?\\w{2})$") && endLookup == '>' )){
+					t = "estamos à procura do dono da matricula e se a mesma existe....  " + plates.get(plateLookup);
+					if(plates.get(plateLookup) == null){
+						t += System.lineSeparator() + " nao existe a matricula " + plateLookup + " registada.";
+					}
+					else{
+						t += System.lineSeparator() + "o dono da matricula: " + plates.get(plateLookup);						}	
+				}
+			}
+			else if( (s.charAt(0) != 'L') && (s.charAt(0) != 'R')){
+				echo("mensagem recebida do cliente, com erro de syntax : " + s);
+				t += " a ligar com o servidor...., mensagem com erro de syntax";
+			}			
+			
+				
+						
+
+			DatagramPacket dp = new DatagramPacket(t.getBytes() , t.getBytes().length , mcastAddr, mcastPORT);
+			socket.send(dp);
+		}
+	}
+
+	public static void echo(String msg)
+	{
+		System.out.println(msg);
+	}
 }
