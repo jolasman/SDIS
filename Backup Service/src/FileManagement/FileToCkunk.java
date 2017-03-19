418aeeafeb060c6a920bfc609dc1883bc43981ed
Joel Carneiro <jolasman@hotmail.com>
2017-03-19 00:11:08 +0000

    experiencias com o algoritmo sha256

--- Backup Service/src/FileManagement/FileToCkunk.java
+++ Backup Service/src/FileManagement/FileToCkunk.java
@@ -1,37 +1,52 @@
 package FileManagement;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
 
 public class FileToCkunk {
 
-	public FileToCkunk(File file) throws IOException {
-		// TODO Auto-generated constructor stub
+	public ArrayList<File> chunks = new ArrayList<File>();
+	public String fileID;
+
+	public FileToCkunk(File file) throws IOException, NoSuchAlgorithmException {
+		
 		int partCounter = 1;	
 		int sizeOfFiles = 64000;
 		byte[] buffer = new byte[sizeOfFiles];
 		
+		//sha256 algorithm ****************************************
+		MessageDigest md = MessageDigest.getInstance( "SHA-512" );
+		md.update( file.getName().getBytes("UTF-8"));
+		byte[] aMessageDigest = md.digest();
+
+		String sha256 = Base64.getEncoder().encodeToString( aMessageDigest );
+		fileID = sha256.substring(0,10);
+		//end *************************************************************
+		
 
 		try (BufferedInputStream file_data = new BufferedInputStream(new FileInputStream(file))) {
-			System.out.println("aceder a dados do file");
+
 			String name = file.getName();
 			int tmp = 0;
 
 			while ((tmp = file_data.read(buffer)) > 0) {
 				//write each chunk of data into separate file with different number in name
-				
-				File newChunk = new File("./Files", name + "." + String.format("%03d", partCounter++));
 
+				File newChunk = new File("./Files", fileID + String.format("%03d", partCounter++));
+				
 				try (FileOutputStream out = new FileOutputStream(newChunk)) {
 					out.write(buffer, 0, tmp); //tmp is chunk size
-					
+					System.out.println("Chunk " + newChunk + " created");
+					chunks.add(newChunk);
 				}
 				catch (IOException e) {
 					System.out.println("Error when we try to write into a new Chunk");
@@ -45,8 +60,14 @@ public class FileToCkunk {
 
 		}
 	}
-	
-	
+
+	public ArrayList<File> getChunks(){
+		return chunks;
+	}
+
+	public String getFileID(){
+		return fileID;
+	}
 }
 
 
--- Backup Service/src/Interface/Interface.java
+++ Backup Service/src/Interface/Interface.java
@@ -2,6 +2,7 @@ package Interface;
 import java.awt.List;
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,7 +11,9 @@ import FileManagement.MergeChunks;;
 
 public class Interface {
 
-	public static void main(String[] args) throws IOException {
+	
+	
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 		/*	if (args.length != 3) {
 			System.out.println("Usage: java Server <srvc_port> <mcast_addr> <mcast_port>");
 			return;
@@ -34,66 +37,11 @@ public class Interface {
 			menuItem = in.nextInt();
 			switch (menuItem) {
 			case 1:
-				File file = new File("./Files/imagem.jpg");
-				File f1 = new File("./Files/imagem.jpg.001");
-				File f2 = new File("./Files/imagem.jpg.002");
-				File f3 = new File("./Files/imagem.jpg.003");
-				File f4 = new File("./Files/imagem.jpg.004");
-				File f5 = new File("./Files/imagem.jpg.005");
-				File f6 = new File("./Files/imagem.jpg.006");
-				File f7 = new File("./Files/imagem.jpg.007");
-				File f8 = new File("./Files/imagem.jpg.008");
 				
-				File f9 = new File("./Files/imagem.jpg.009");
-				File f10 = new File("./Files/imagem.jpg.010");
-				File f11 = new File("./Files/imagem.jpg.011");
-				File f12 = new File("./Files/imagem.jpg.012");
-				File f13 = new File("./Files/imagem.jpg.013");
-				File f14 = new File("./Files/imagem.jpg.014");
-				File f15 = new File("./Files/imagem.jpg.015");
-				File f16 = new File("./Files/imagem.jpg.016");
-				File f17 = new File("./Files/imagem.jpg.017");
-				File f18 = new File("./Files/imagem.jpg.018");
-				File f19 = new File("./Files/imagem.jpg.019");
-				File f20 = new File("./Files/imagem.jpg.020");
-				File f21 = new File("./Files/imagem.jpg.021");
-				File f22 = new File("./Files/imagem.jpg.022");
-				File f23 = new File("./Files/imagem.jpg.023");
-				File f24 = new File("./Files/imagem.jpg.024");
-				File f25 = new File("./Files/imagem.jpg.025");
-				File f26 = new File("./Files/imagem.jpg.026");
+				File file_initiator = new File("./Files/imagem.jpg");
+				FileToCkunk initial_file = new FileToCkunk(file_initiator);
+				MergeChunks merged_file = new MergeChunks(initial_file.getChunks(),new File("./Files/file_merged.jpg"));
 				
-				File exper = new File("./Files/exp.jpg");
-				FileToCkunk exp = new FileToCkunk(file);
-				ArrayList<File> myList = new ArrayList<File>();
-				myList.add(f1);
-				myList.add(f2);
-				myList.add(f3);
-				myList.add(f4);
-				myList.add(f5);
-				myList.add(f6);
-				myList.add(f7);
-				myList.add(f8);
-				myList.add(f9);
-				myList.add(f10);
-				myList.add(f11);
-				myList.add(f12);
-				myList.add(f13);
-				myList.add(f14);
-				myList.add(f15);
-				myList.add(f16);
-				myList.add(f17);
-				myList.add(f18);
-				myList.add(f19);
-				myList.add(f20);
-				myList.add(f21);
-				myList.add(f22);
-				myList.add(f23);
-				myList.add(f24);
-				myList.add(f25);
-				myList.add(f26);
-				MergeChunks merge = new MergeChunks(myList,exper);
-			
 				// do something...
 				break;
 			case 2:
